d3540c4059778e02be7e73e171ce3ad4c9acf340
banthab0mb <hrudowski0882@gmail.com>
2024-08-23 15:44:39 -0400

    Added exponential control for driving Needs testing, percents don't work to it is outputting RPM

--- src/main.cpp
+++ src/main.cpp
@@ -100,11 +100,19 @@ void usercontrol(void)
   BackRightMotor.stop();
   MiddleRightMotor.stop();
 
-
   // User control code here, inside the loop
   while (1)
-  {
+  { 
+        // x₁ = Left Motor X = [Controller] [3] position + [Controller] [1] position
+        // x₂ = Right Motor X = [Controller] [3] position - [Controller] [1] position
+        // Sets each motor's velocity (in rpm) to 0.0001x³ (shown as 0.0001 * x * x * x)
+        FrontLeftMotor.set_velocity_custom_controller((0.0001 * (((Controller1.Axis3.position() + Controller1.Axis1.position()) * (Controller1.Axis3.position() + Controller1.Axis1.position())) * (Controller1.Axis3.position() + Controller1.Axis1.position()))) /*, percent*/ );
+        BackLeftMotor.set_velocity_custom_controller((0.0001 * (((Controller1.Axis3.position() + Controller1.Axis1.position()) * (Controller1.Axis3.position() + Controller1.Axis1.position())) * (Controller1.Axis3.position() + Controller1.Axis1.position()))) /*, percent */);
+        FrontRightMotor.set_velocity_custom_controller((0.0001 * (((Controller1.Axis3.position() - Controller1.Axis1.position()) * (Controller1.Axis3.position() - Controller1.Axis1.position())) * (Controller1.Axis3.position() - Controller1.Axis1.position()))) /*, percent */);
+        BackRightMotor.set_velocity_custom_controller((0.0001 * (((Controller1.Axis3.position() - Controller1.Axis1.position()) * (Controller1.Axis3.position() - Controller1.Axis1.position())) * (Controller1.Axis3.position() - Controller1.Axis1.position()))) /*, percent */);
+   
   
+   
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
