f1fdac585acb3c6343acc24aa6afd7d8c50309a8
banthab0mb <hrudowski0882@gmail.com>
2024-10-04 11:14:13 -0400

    fixed naming

--- src/pre-auton.cpp
+++ src/pre-auton.cpp
@@ -12,7 +12,7 @@ Button autonButtons[] = {
   Button(10, 10, 150, 50, "Left MOGO", green, black),
   Button(170, 10, 150, 50, "Left WALL", white, black),
   Button(10, 70, 150, 50, "Right MOGO", white, black),
-  Button(170, 70, 150, 50, "Right STAKE", white, black)
+  Button(170, 70, 150, 50, "Right WALL", white, black)
 };
 
 
